ef2e48f9e2a60d2339ce62321a49e7e8ce57f46f
jofloresp <jofloresp@canvia.com>
2021-05-11 18:09:59 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PushNotificationService} from './services/push-notification.service';
+import {INotificationPayload} from './services/notificacion-interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularNotificacionesFirebase';
+  mesaggeReceived = '';
+  messageBody = '';
+
+  constructor(private notificacion: PushNotificationService) {
+    notificacion.requestPermission().then(token => {
+      console.log(token);
+    });
+  }
+
+  ngOnInit(): void {
+    this.notificacion.receiveMessage().subscribe(payload => {
+      console.log(payload);
+      this.mesaggeReceived = payload.notification.title;
+      this.messageBody = payload.notification.body;
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDHessgsbcva6Thmbkh2jk4RBRBtvg4FXk',
+    authDomain:  'sistema-nivel-de-madurez.firebaseapp.com',
+    databaseURL: 'https://sistema-nivel-de-madurez-default-rtdb.firebaseio.com',
+    projectId: 'sistema-nivel-de-madurez',
+    storageBucket: 'sistema-nivel-de-madurez.appspot.com',
+    messagingSenderId: '733912323924',
+    appId: '1:733912323924:web:962a1b189decb28f0b5ac1',
+    measurementId: 'G-Z4GMT6PZF4'
+  }
 };
 
 /*
